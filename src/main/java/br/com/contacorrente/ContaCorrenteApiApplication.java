4d1ad91505277ba91fae62af0a7577d76f233a81
Levy Carvalho Melo <levycm@gmail.com>
2021-01-31 02:02:43 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/br/com/contacorrente/ContaCorrenteApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.contacorrente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaCorrenteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaCorrenteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/contacorrente/ContaCorrenteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.contacorrente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaCorrenteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
